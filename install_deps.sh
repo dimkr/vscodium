d5f3215c13fde616365281908ac3e5226e9b18b3
Dima Krasner <dima@dimakrasner.com>
2019-12-06 08:33:03 +0200

    Force installation of arm64 packages

--- install_deps.sh
+++ install_deps.sh
@@ -11,8 +11,9 @@ else
     echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ bionic main" | sudo tee -a /etc/apt/sources.list.d/arm64.list >/dev/null
     sudo dpkg --add-architecture arm64
     sudo apt-get update
-    sudo apt-get install libc6-dev-arm64-cross gcc-aarch64-linux-gnu g++-aarch64-linux-gnu pkg-config-aarch64-linux-gnu
-    sudo apt-get install libx11-dev:arm64 libxkbfile-dev:arm64
+    sudo apt-get install libc6-dev-arm64-cross gcc-aarch64-linux-gnu g++-aarch64-linux-gnu pkg-config-aarch64-linux-gnu x11proto-dev
+    apt-get download libx11-dev:arm64 libx11-6:arm64 libxkbfile-dev:arm64 libxkbfile1:arm64 libxau-dev:arm64 libxdmcp-dev:arm64 libxcb1-dev:arm64
+    for i in *.deb; do ar xv $i; tar -C / -xvf data.tar.*; rm -f data.tar.*; done
     export CC=/usr/bin/aarch64-linux-gnu-gcc
     export CXX=/usr/bin/aarch64-linux-gnu-g++
     export CC_host=/usr/bin/gcc
