2fe77db146825c58f07339add31eb57afaa34136
Dima Krasner <dima@dimakrasner.com>
2019-12-06 10:10:13 +0200

    Install all X11 protocol headers

--- install_deps.sh
+++ install_deps.sh
@@ -8,10 +8,10 @@ else
   sudo apt-get install -y fakeroot rpm jq
   if [[ $BUILDARCH == "arm64" ]]; then
     sed 's/^deb /deb [arch=amd64] '/g -i /etc/apt/sources.list
-    echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ bionic main" | sudo tee -a /etc/apt/sources.list.d/arm64.list >/dev/null
+    echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ trusty main" | sudo tee -a /etc/apt/sources.list.d/arm64.list >/dev/null
     sudo dpkg --add-architecture arm64
     sudo apt-get update
-    sudo apt-get install libc6-dev-arm64-cross gcc-aarch64-linux-gnu g++-aarch64-linux-gnu pkg-config-aarch64-linux-gnu x11proto-dev
+    sudo apt-get install libc6-dev-arm64-cross gcc-aarch64-linux-gnu g++-aarch64-linux-gnu pkg-config-aarch64-linux-gnu `apt-cache search x11proto | grep ^x11proto | cut -f 1 -d ' '` xz-utils
     apt-get download libx11-dev:arm64 libx11-6:arm64 libxkbfile-dev:arm64 libxkbfile1:arm64 libxau-dev:arm64 libxdmcp-dev:arm64 libxcb1-dev:arm64
     for i in *.deb; do ar xv $i; sudo tar -C / -xvf data.tar.*; rm -f data.tar.*; done
     export CC=/usr/bin/aarch64-linux-gnu-gcc
