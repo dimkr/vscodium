c8ce97a28ed49a2bb75bba9b7f8fee53e800c773
Dima Krasner <dima@dimakrasner.com>
2020-10-10 11:55:05 +0300

    Restore CI/CD from Ubuntu 14.04 on Travis

---
+++ create_dmg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+if [[ "$SHOULD_BUILD" == "yes" ]]; then
+  if [[ "$OS_NAME" == "osx" ]]; then
+    cd VSCode-darwin
+    npx create-dmg VSCodium.app ..
+    mv "../VSCodium ${LATEST_MS_TAG}.dmg" "../VSCodium.${LATEST_MS_TAG}.dmg"
+  fi
+  cd ..
+fi
